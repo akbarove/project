64cf0ee3acd4c0cdd2b6804fa0e11888bb5d058c
Erlan <akbaroverlanicloud.com@MacBook-Pro-Erlan.local>
2024-04-02 16:47:10 +0600

    .

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../widgets/navbar/Navbar";
+import Input from "../widgets/input/Input";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Input/>
     </div>
   );
 };
