247e703c004f142248c36fd93636ec31b549c89b
melolidas <the_halidcastle@mail.ru>
2024-04-02 17:11:04 +0600

    .

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../widgets/navbar/Navbar";
 import Input from "../widgets/input/Input";
+import MainRoutes from "./routes/MainRoutes";
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      <Input/>
+      <MainRoutes />
+      <Input />
     </div>
   );
 };
